dfaf7d3afeb90e4befc37c8c61074a1a87a23908
Ling Ip <ling@tripactions.com>
2022-05-15 17:25:27 -0700

    json

--- foundation/json/src/main/java/org/example/jwt/json/config/JsonConfig.java
+++ foundation/json/src/main/java/org/example/jwt/json/config/JsonConfig.java
@@ -1,5 +1,9 @@
 package org.example.jwt.json.config;
 
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,5 +13,16 @@ public class JsonConfig {
     @Bean
     public Module javaTime() {
         return new JavaTimeModule();
+    }
+
+    public ObjectMapper createObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true);
+        objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+        objectMapper.setDefaultPrettyPrinter(new DefaultPrettyPrinter());
+        objectMapper.registerModules(new JavaTimeModule());
+        return objectMapper;
     }
 }
